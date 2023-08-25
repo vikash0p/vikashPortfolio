b7c8d5865dc6261d88ba9276489c382614e32c69
Vikash <vikash2000158@gmail.com>
2023-08-25 12:44:52 +0530

    router

--- components/Animationpage.js
+++ components/Animationpage.js
@@ -3,12 +3,10 @@ import { AnimatePresence, motion } from 'framer-motion'
 import React from 'react'
 import Transiton from './Transiton'
 
-
 import { useRouter } from 'next/router';
 
 const Animationpage = ({ children }) => {
     const router=useRouter();
-    // console.log(router)
     return (
         <AnimatePresence mode='wait'>
             <motion.div key={router.route} className='h-full' >
