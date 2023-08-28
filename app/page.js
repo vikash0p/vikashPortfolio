c197844ab976b7ca740549b5e7dd96c69e768bde
vikash0p <vikash2000158gmail.com>
2023-08-28 20:58:47 +0530

    remove metadata from page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
 import { HomeLeft, HomeRight } from "@/components"
 
 
-export const metadata = {
-  title: 'Home',
-  description: '👋 Welcome to My Portfolio! 🚀 I\'m a dedicated Front- End Developer who turns ideas into captivating web experiences.With a blend of creativity and code, I craft user - friendly interfaces using HTML, CSS, and JavaScript.Explore my projects that showcase my passion for design and innovation.Let\'s build the web of tomorrow, today!'
-}
+
 
 const Home = () => {
   return (
