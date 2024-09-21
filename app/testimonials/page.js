5409facddcee62c088c20e51a51b92a32f346f9b
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-21 19:11:11 +0530

    add the metadata

--- app/testimonials/page.js
+++ app/testimonials/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 
 export const metadata = {
-  title: 'About',
+  title: 'Testimonials',
   description: 'Client Love: 💬 Here\'s what amazing individuals I\'ve had the privilege to work with have to say  Exceptional eye html design and seamless functionality! Turned our vision into an interactive masterpiece. Reliable, creative, and a joy to collaborate with. Let\'s create something incredible together!'
 }
 
