c92908d8d19c4329f8a1b4ed565edc014bcee284
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-23 13:56:36 +0530

    add the data in service page router

--- app/services/page.js
+++ app/services/page.js
@@ -1,10 +1,21 @@
+import ProfileCardComponent from '@/components/ReusableComponent/ProfileCardComponent'
+import TopTitle from '@/components/ReusableComponent/TopTitle'
 import React from 'react'
 
 const Services = () => {
   return (
-    <div>
-      
-    </div>
+      <section>
+          <TopTitle title="Services" />
+          <div className='w-full mt-14 flex flex-col lg:flex-row gap-5'>
+              <div className=" lg:w-1/4 ">
+                  <ProfileCardComponent />
+
+              </div>
+              <div className="lg:w-3/4">
+
+              </div>
+          </div>
+      </section>
   )
 }
 
