9dc4ad17c91d4e3ffeb504a8428b4b7e5bef6731
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-23 13:01:05 +0530

    add the social media in profileCardComponent

--- components/ReusableComponent/ProfileCardComponent.js
+++ components/ReusableComponent/ProfileCardComponent.js
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import React from 'react';
+import SocialMedia from './SocialMedia';
 
 const ProfileCardComponent = () => {
     return (
@@ -13,7 +14,7 @@ const ProfileCardComponent = () => {
         >
             <motion.div className="flex items-center ">
                 <motion.div
-                    className="w-full h-72 relative  overflow-hidden border-2 border-gray-300"
+                    className="w-full h-96 lg:h-72 relative  overflow-hidden border-2 border-gray-300"
                     initial={{ scale: 0.8 }}
                     animate={{ scale: 1 }}
                     transition={{ duration: 0.6 }}
@@ -23,50 +24,52 @@ const ProfileCardComponent = () => {
                         alt="Profile Picture"
                         layout="fill"
                         objectFit="cover"
+                        className='w-full h-full object-cover'
                     />
                 </motion.div>
 
             </motion.div>
-<motion.div
-    className="px-5 py-4 text-lg back1"
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ delay: 0.5, duration: 0.6 }}
->
-    <div className="space-y-3">
-        <p><strong className=''>Name:</strong> Vikash Pandat</p>
+            <motion.div
+                className="px-5 py-4 text-lg back1"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.5, duration: 0.6 }}
+            >
+                <div className="space-y-3">
+                    <p><strong className=''>Name:</strong> Vikash Pandat</p>
 
-        <p>
-            <strong>Whatsapp: </strong>
-            <a href="https://wa.me/919990127407" className=" hover:underline">
-                 +91-9990127407
-            </a>
-        </p>
+                    <p>
+                        <strong>Whatsapp: </strong>
+                        <a href="https://wa.me/919990127407" className=" hover:underline">
+                            +91-9990127407
+                        </a>
+                    </p>
 
-        <p>
-            <strong>Phone: </strong>
-            <a href="tel:+918448925560" className=" hover:underline">
-                +91-8448925560
-            </a>
-        </p>
+                    <p>
+                        <strong>Phone: </strong>
+                        <a href="tel:+918448925560" className=" hover:underline">
+                            +91-8448925560
+                        </a>
+                    </p>
 
-        <p>
-            <strong>Email: </strong>
-            <a href="mailto:vikash752200@gmail.com" className=" hover:underline">
-                Vikash752200@gmail.com
-            </a>
-        </p>
+                    <p>
+                        <strong>Email: </strong>
+                        <a href="mailto:vikash752200@gmail.com" className=" hover:underline">
+                            Vikash752200@gmail.com
+                        </a>
+                    </p>
 
-        <p><strong>Experience:</strong> 2+ years</p>
+                    <p><strong>Experience:</strong> 2+ years</p>
 
-        <p>
-            <strong>Available:</strong>
-            <span> Yes</span>
-        </p>
+                    <p>
+                        <strong>Available:</strong>
+                        <span> Yes</span>
+                    </p>
 
-        <p><strong>Nationality:</strong> India</p>
-    </div>
-</motion.div>
+                    <p><strong>Nationality:</strong> India</p>
+                    <SocialMedia />
+                </div>
+            </motion.div>
 
         </motion.div>
     );
