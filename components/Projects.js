a9a86ba8fa62789484e2dbe67eecbedfc70a8bc6
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-25 13:06:57 +0530

    complete the project page

--- components/Projects.js
+++ components/Projects.js
@@ -3,60 +3,73 @@ import { project } from '@/data'
 import { fadeIn } from '@/variants'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { FiArrowRight } from 'react-icons/fi';
 
 
 const Projects = () => {
-  return (
+    return (
 
-          <>
+        <>
 
-              <div className='grid w-full  mt-20 grid-cols-1 gap-y-10 gap-x-5   md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-                  {
-                      project.map((value, index) => {
-                          return (
-                              <motion.div
-                                  variants={fadeIn("up", 0.7)}
-                                  initial="hidden"
-                                  whileInView="show"
-                                  viewport={{
-                                      once: true,
-                                      amount: 0.6
-                                  }}
-                              key={index} className='relative overflow-hidden cursor-pointer rounded-xl group'>
+            <div className='grid w-full  mt-20 grid-cols-1 gap-y-10 gap-x-5   md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+                {
+                    project.map((value, index) => {
+                        return (
+                            <motion.div
+                                variants={fadeIn("up", 0.7)}
+                                initial="hidden"
+                                whileInView="show"
+                                viewport={{
+                                    once: true,
+                                    amount: 0.6
+                                }}
+                                key={index} className='relative overflow-hidden cursor-pointer  group'>
 
-                                  <Image
-                                      src={value.image}
-                                      alt={value.link}
-                                      width={1000}
-                                      height={1000}
-                                      className='w-[380px] h-[380px] object-cover rounded-xl '
-                                      sizes='400px'
-                                      priority={true}
-                                  />
+                                <div className='relative w-full max-w-sm h-96'>
+                                    <Image
+                                        src={value.image}
+                                        alt={value.link}
+                                        fill
+                                        className='w-full h-full object-cover object-top '
+                                        sizes='400px'
+                                        priority={true}
+                                        quality={100}
+                                    />
 
-
-
-                                  <div className='absolute inset-0 back1 opacity-0 group-hover:opacity-90 transition-all duration-700  '>
-                                      <a href={value.link} target='_blank' className='m-4 text-lg' >Go to Project</a>
+                                </div>
 
 
-                                  </div>
-                                  <div className='absolute bottom-0 transition-all duration-300 translate-y-full group-hover:-translate-y-10 group-hover:lg:-translate-y-20'>
-                                      <h1 className='text-2xl'>
-                                          {value.title}
-                                      </h1>
-                                  </div>
+
+                                <div className='absolute bottom-0 left-0 right-0 h-12 group-hover:h-28 transition-all duration-500'>
+                                    {/* Background with opacity */}
+                                    <div className='absolute inset-0 opacity-80 back1 transition-all duration-500 group-hover:h-28'></div>
+
+                                    {/* Foreground content (Text and Link) */}
+                                    <div className='relative py-3 flex flex-col items-center gap-3'>
+                                        <p className='text-xl uppercase'>{value.title}</p>
+                                        <div className='opacity-0 group-hover:opacity-100 transition-all duration-700'>
+                                            <Link href={value.link} target='_blank' className='back2 px-5 py-2 rounded-sm flex items-center gap-2'>
+                                                Go to Project
+                                                {/* React Icon for the arrow */}
+                                                <FiArrowRight className="text-xl group-hover:translate-x-3 transition-all duration-300" />
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
 
 
-                              </motion.div>
 
-                          )
-                      })
-                  }
-              </div>
-          </>
-  )
+
+                            </motion.div>
+
+                        )
+                    })
+                }
+            </div>
+        </>
+    )
 }
 
 export default Projects
--- data.js
+++ data.js
@@ -11,8 +11,8 @@ export const navData = [
   { name: 'Home', path: '/', icon: <HiHome /> },
   { name: 'About', path: '/about', icon: <HiUser /> },
   { name: 'Services', path: '/services', icon: <FaServicestack /> },
-  { name: 'work', path: '/work', icon: <FaProjectDiagram /> },
-  { name: 'Blog', path: '/blog', icon: <FaBlog /> },
+  { name: 'Projects', path: '/work', icon: <FaProjectDiagram /> },
+  { name: 'Blogs', path: '/blog', icon: <FaBlog /> },
 
   { name: 'contact', path: '/contact', icon: <IoIosContact />,},
 ];
@@ -84,52 +84,6 @@ export const social = [
 
 ]
 
-export const project = [
-  {
-    id: 1,
-    image: '/avtar.jpg',
-    link: 'https://portfoliopro-snowy.vercel.app/',
-    title: 'Creating, innovating, problem-solving  designing a brighter future, one idea at a time.',
-  },
-  {
-    id: 2,
-    image: '/grocery.jpg',
-    link: 'https://groceryapp-ten.vercel.app/',
-    title: 'Fresh produce, pantry essentials, and delightful treats - your neighborhood grocery.',
-  },
-  {
-    id: 3,
-    image: '/hotal.jpg',
-    link: 'https://hotals.vercel.app/',
-    title: "Luxurious rooms, impeccable service, breathtaking views - your unforgettable hotel experience.",
-  },
-  {
-    id: 4,
-    image: '/netflix.jpg',
-    link: 'https://netflixapp-rho.vercel.app/',
-    title: 'Unlimited entertainment: movies, series, documentaries - streaming joy html every moment.',
-  },
-  {
-    id: 5,
-    image: '/resturnat.jpg',
-    link: 'https://vercel.com/vikash0p/resturant',
-    title: 'Savor exquisite flavors, impeccable service, and warm ambiance at our culinary haven.',
-  },
-  {
-    id: 6,
-    image: '/shop.jpg',
-    link: 'https://showfit.netlify.app/',
-    title: 'Discover trends, find deals, and shop your favorites - retail therapy awaits.',
-  },
-  {
-    id: 7,
-    image: '/vpn.jpg',
-    link: 'https://lasvpn.vercel.app/',
-    title: 'Secure browsing, anonymity assured - your online world, privately accessible with VPN.',
-  },
-
-]
-
 export const testimonialData = [
   {
     image: '/images/t-avt-1.png',
@@ -174,3 +128,107 @@ export const testimonialData = [
       'Their ability to take complex product ideas and turn them into tangible results is amazing. The team is not only technically proficient but also great at adapting to changing requirements throughout the project.',
   },
 ];
+
+
+
+export const project = [
+  {
+    id: 1,
+    image: '/project/blog.png',
+    link: 'https://frontendblog-nu.vercel.app',
+    title: 'modern blog',
+  },
+  {
+    id: 2,
+    image: '/project/tattoo.png',
+    link: 'https://karmatattoo.vercel.app',
+    title: 'Fresh produce',
+  },
+  {
+    id: 3,
+    image: '/project/pizza.png',
+    link: 'https://frontendpizza.vercel.app',
+    title: "hop pizza",
+  },
+  {
+    id: 4,
+    image: '/project/fashionStore.png',
+    link: 'https://storefashion.vercel.app',
+    title: 'fashion store',
+  },
+  {
+    id: 5,
+    image: '/project/eligentjewllery.png',
+    link: 'https://jewllery-eight.vercel.app',
+    title: 'elegant jewellery',
+  },
+  {
+    id: 6,
+    image: '/project/mantrayoga.png',
+    link: 'https://yoga-gilt-beta.vercel.app',
+    title: 'mantra yoga',
+  },
+  {
+    id: 7,
+    image: '/project/delicuious.png',
+    link: 'https://hotals.vercel.app',
+    title: 'Delicious food',
+  },
+  {
+    id: 8,
+    image: '/project/laslas.png',
+    link: 'https://lasvpn.vercel.app',
+    title: 'laslas vpn',
+  },
+  {
+    id: 9,
+    image: '/project/portfoioone.png',
+    link: 'https://portfolio-silk-rho-11.vercel.app',
+    title: 'personal portfolio',
+  },
+  {
+    id: 10,
+    image: '/project/barshop.png',
+    link: 'https://barshop.vercel.app',
+    title: 'bar shop',
+  },
+  {
+    id: 11,
+    image: '/project/fruitShop.png',
+    link: 'https://fruitsshop.vercel.app/shop',
+    title: 'Fruits shop',
+  },
+  {
+    id: 12,
+    image: '/project/grocery.png',
+    link: 'https://groceryapp-ten.vercel.app',
+    title: 'Grocery shop',
+  },
+  {
+    id: 13,
+    image: '/project/showfit.png',
+    link: 'https://fabfit.netlify.app',
+    title: 'ShowFit',
+  },
+  {
+    id: 14,
+    image: '/project/youotube.png',
+    link: 'https://youtebe.netlify.app',
+    title: 'youtube clone',
+  },
+  {
+    id: 15,
+    image: '/project/protein.png',
+    link: 'https://protein.netlify.app/shop',
+    title: 'Fitness club',
+  },
+  {
+    id: 16,
+    image: '/project/todo.png',
+    link: 'https://tdoo.netlify.app',
+    title: 'todo app',
+  },
+
+]
+
+
