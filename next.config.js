bbb58118fda821bdd7cfc2e9727d10048fed6246
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-26 17:45:01 +0530

    add the images in next js config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-    formats:['image/avif','image/webp'],
+        
+            deviceSizes: [320, 480, 640, 768, 1024, 1280, 1440, 1600, 1920],
+            imageSizes: [64, 128, 256, 384, 512, 768, 1024],
+            formats: ['image/avif', 'image/webp'],
     }
 }
 
