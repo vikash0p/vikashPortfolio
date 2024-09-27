175999955e8ddf796f444a9ac47a3a1efaa03e35
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-27 20:00:57 +0530

    add the data in sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,22 +1,42 @@
 export default function sitemap() {
+    const currentDate = new Date().toISOString(); // Generate current date
+
     return [
         {
-            url: 'https://acme.com',
-            lastModified: new Date(),
-            changeFrequency: 'yearly',
-            priority: 1,
+            url: 'https://myportfolio-navy-three.vercel.app/',
+            lastModified: currentDate,
+            changeFrequency: 'yearly', // Assuming yearly changes
+            priority: 0.4,
         },
         {
-            url: 'https://acme.com/about',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8,
+            url: 'https://myportfolio-navy-three.vercel.app/about',
+            lastModified: currentDate,
+            changeFrequency: 'monthly', // Assuming monthly updates
+            priority: 1.0,
         },
         {
-            url: 'https://acme.com/blog',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.5,
+            url: 'https://myportfolio-navy-three.vercel.app/services',
+            lastModified: currentDate,
+            changeFrequency: 'monthly', // Assuming monthly updates
+            priority: 1.0,
         },
-    ]
+        {
+            url: 'https://myportfolio-navy-three.vercel.app/work',
+            lastModified: currentDate,
+            changeFrequency: 'monthly', // Assuming monthly updates
+            priority: 1.0,
+        },
+        {
+            url: 'https://myportfolio-navy-three.vercel.app/blog',
+            lastModified: currentDate,
+            changeFrequency: 'weekly', // Blogs might update more frequently
+            priority: 1.0,
+        },
+        {
+            url: 'https://myportfolio-navy-three.vercel.app/contact',
+            lastModified: currentDate,
+            changeFrequency: 'yearly', // Assuming minimal changes to contact page
+            priority: 1.0,
+        },
+    ];
 }
