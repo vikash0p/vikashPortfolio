4cd17a4b3c948c851862b66e08728cd05b77a9cd
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-27 07:40:16 +0530

    add the loading

---
+++ app/loading.js
@@ -0,0 +1,7 @@
+'use client'
+export default function Loading() {
+    // Or a custom loading skeleton component
+    return <div className="w-full min-h-screen flex flex-col gap-4 items-center justify-center ">
+        <div class="loader"></div>
+    </div>
+}
