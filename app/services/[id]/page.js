5662e1954a3186a0198bc7a69130a25d167de026
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-27 17:55:40 +0530

    add the dynamic metadata fro service data

--- app/services/[id]/page.js
+++ app/services/[id]/page.js
@@ -10,6 +10,46 @@ export async function generateStaticParams() {
   }));
 }
 
+export async function generateMetadata({ params }) {
+  const { id } = params;
+
+  // Find the service item by id from the data
+  const service = mysServiceData.find(item => item.value === id);
+  // console.log("🚀 ~ file: page.js:18 ~ service:", service);
+
+  if (service) {
+    return {
+      title: `${service.title} | Vikash Services`,
+      description: service.description,
+      openGraph: {
+        title: `${service.title} | Vikash Services`,
+        description: service.description,
+        url: `https://myportfolio-navy-three.vercel.app/services/${id}`,
+        images: [
+          {
+            url: `${service.image}`,
+            width: 1200,
+            height: 630,
+            alt: `${service.title} - Service`,
+          },
+        ],
+        siteName: 'Vikash Services',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        title: `${service.title} | Vikash Services`,
+        description: service.description,
+        images: [`${service.image}`], // Replace with correct URL
+      },
+    };
+  } else {
+    return {
+      title: `Service Not Found | Vikash Services`,
+      description: `This service could not be found.`,
+    };
+  }
+}
+
 const ServiceId = ({params}) => {
     const {id}=params;
   return (
