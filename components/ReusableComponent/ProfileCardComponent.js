006d5f991acaac0a99a13fab7865547ba0aeea17
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-10-01 16:53:41 +0530

    chage the font size

--- components/ReusableComponent/ProfileCardComponent.js
+++ components/ReusableComponent/ProfileCardComponent.js
@@ -37,7 +37,7 @@ const ProfileCardComponent = () => {
                 animate={{ opacity: 1 }}
                 transition={{ delay: 0.5, duration: 0.6 }}
             >
-                <div className="space-y-1 text-lg">
+                <div className="space-y-1 text-base md:text-lg">
                     <p><strong className=''>Name:</strong> Vikash Pandat</p>
 
                     <p>
