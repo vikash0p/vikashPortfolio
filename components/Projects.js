fbede5be8354a1bbe587dbed1f3682d79ef798b0
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-10-01 13:27:19 +0530

    add the background in project

--- components/Projects.js
+++ components/Projects.js
@@ -17,7 +17,7 @@ const Projects = () => {
 
         <>
 
-            <div className='grid w-full  mt-20 grid-cols-1 gap-y-10 gap-x-5   md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            <div className='grid w-full grid-cols-1 mt-20 gap-y-10 gap-x-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
                 {
                     project.map((value, index) => {
                         return (
@@ -29,14 +29,14 @@ const Projects = () => {
                                     once: true,
                                     amount: 0.6
                                 }}
-                                key={index} className='relative overflow-hidden cursor-pointer  group'>
+                                key={index} className='relative overflow-hidden cursor-pointer back2 group'>
 
                                 <div className='relative w-full max-w-sm h-[500px] lg:h-96'>
                                     <Image
                                         src={value.image}
                                         alt={value.link}
                                         fill
-                                        className='w-full h-full object-cover object-top '
+                                        className='object-cover object-top w-full h-full '
                                         sizes="(min-width: 1280px) 305px, (min-width: 1040px) calc(23.18vw + 92px), (min-width: 420px) 384px, calc(92vw + 16px)"
                                         quality={75}
                                         loading='eager'
@@ -48,18 +48,18 @@ const Projects = () => {
 
 
 
-                                <div className='absolute bottom-0 left-0 right-0 h-12 group-hover:h-28 transition-all duration-500'>
+                                <div className='absolute bottom-0 left-0 right-0 h-12 transition-all duration-500 group-hover:h-28'>
                                     {/* Background with opacity */}
-                                    <div className='absolute inset-0 opacity-80 back1 transition-all duration-500 group-hover:h-28'></div>
+                                    <div className='absolute inset-0 transition-all duration-500 opacity-80 back1 group-hover:h-28'></div>
 
                                     {/* Foreground content (Text and Link) */}
-                                    <div className='relative py-3 flex flex-col items-center gap-3'>
+                                    <div className='relative flex flex-col items-center gap-3 py-3'>
                                         <p className='text-xl uppercase'>{value.title}</p>
-                                        <div className='opacity-0 group-hover:opacity-100 transition-all duration-700'>
-                                            <Link href={value.link} target='_blank' className='back2 px-5 py-2 rounded-sm flex items-center gap-2'>
+                                        <div className='transition-all duration-700 opacity-0 group-hover:opacity-100'>
+                                            <Link href={value.link} target='_blank' className='flex items-center gap-2 px-5 py-2 rounded-sm back2'>
                                                 Go to Project
                                                 {/* React Icon for the arrow */}
-                                                <FiArrowRight className="text-xl group-hover:translate-x-3 transition-all duration-300" />
+                                                <FiArrowRight className="text-xl transition-all duration-300 group-hover:translate-x-3" />
                                             </Link>
                                         </div>
                                     </div>
