9909b011b2f994b830e9ceb6ec073249a5a5e11d
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-12-05 19:17:39 +0530

    solved the error second

--- app/about/page.js
+++ app/about/page.js
@@ -2,11 +2,12 @@ import ClientSideAboutLayout from "@/components/aboutPage/ClientSideAboutLayout"
 import { AboutMetaData } from "@/meta/blog/AboutMetaData";
 export const metadata = AboutMetaData;
 
+
+
 const About = () => {
   return (
 
-      <ClientSideAboutLayout />
-
+    <ClientSideAboutLayout />
   )
 }
 
