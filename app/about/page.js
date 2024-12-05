79cb8259d90d40857b6f568a1ab4f24504abd5bb
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-12-05 19:03:15 +0530

    remove add the new component for error in build time

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,6 @@
+import ClientSideAboutLayout from "@/components/aboutPage/ClientSideAboutLayout";
 import { AboutMetaData } from "@/meta/blog/AboutMetaData";
-import dynamic from 'next/dynamic';
-const ClientSideAboutLayout = dynamic(() => import('@/components/aboutPage/ClientSideAboutLayout'));
 export const metadata = AboutMetaData;
-
 
 const About = () => {
   return (
