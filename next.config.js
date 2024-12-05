705ed4b309bfa234cd39ad6ce9f0b5913ac4b0ec
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-12-05 15:20:28 +0530

    add the images for third party

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
         deviceSizes: [320, 480, 640, 768, 1024, 1280, 1440, 1600, 1920],
         imageSizes: [64, 128, 256, 384, 512, 768, 1024],
         formats: ['image/avif', 'image/webp'],
+
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+
+            },
+        ],
     }
 }
 
