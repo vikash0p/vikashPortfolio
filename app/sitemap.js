048ba9ef3502e8b98dde71bd8d75353ee5aff9d5
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-12-31 16:05:01 +0530

    refactor sitemap.js to streamline changeFrequency assignments

--- app/sitemap.js
+++ app/sitemap.js
@@ -11,32 +11,30 @@ export default function sitemap() {
         {
             url: `${process.env.NEXT_PUBLIC_APP_URL}/about`,
             lastModified: currentDate,
-            changeFrequency: 'monthly', // Assuming monthly updates
+            changeFrequency: 'monthly', 
             priority: 1.0,
         },
         {
             url: `${process.env.NEXT_PUBLIC_APP_URL}/services`,
             lastModified: currentDate,
-            changeFrequency: 'monthly', // Assuming monthly updates
-            priority: 1.0,
+            changeFrequency: 'monthly',
         },
         {
             url: `${process.env.NEXT_PUBLIC_APP_URL}/work`,
             lastModified: currentDate,
-            changeFrequency: 'monthly', // Assuming monthly updates
+            changeFrequency: 'monthly',
             priority: 1.0,
         },
         {
             url: `${process.env.NEXT_PUBLIC_APP_URL}/blog`,
             lastModified: currentDate,
-            changeFrequency: 'weekly', // Blogs might update more frequently
+            changeFrequency: 'weekly',
             priority: 1.0,
         },
         {
             url: `${process.env.NEXT_PUBLIC_APP_URL}/contact`,
             lastModified: currentDate,
-            changeFrequency: 'yearly', // Assuming minimal changes to contact page
-            priority: 1.0,
+            changeFrequency: 'yearly',
         },
     ];
 }
